9ce70a83e15689df1299138cac35b48cfa84cd24
PhamTienDat <tdat9663@gmail.com>
2025-07-08 22:35:49 +0700

    show stack trace errror

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -12,7 +12,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   }
 
   // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN Stack trên kênh Youtube: https://www.youtube.com/@trungquandev)
-  if (process.env.BUILD_MODE !== 'dev') delete responseError.stack
+  // if (process.env.BUILD_MODE !== 'dev') delete responseError.stack
 
   // ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...
 
