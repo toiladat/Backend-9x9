e58424a843ee47001f22963d5c555668ca31e602
PhamTienDat <tdat9663@gmail.com>
2025-07-26 12:20:11 +0700

    update password app mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,12 +6,12 @@ const sendVerificationEmail = (to, subject, htmlContent) => {
   const adminEmail = process.env.SEND_MAI_EMAILL
   const adminPassword = process.env.SEND_MAIL_PASSWORD
   const mailHost = 'smtp.gmail.com'
-  const mailPort = 587
+  const mailPort = 465
 
   const transporter = nodeMailer.createTransport({
     host: mailHost,
     port: mailPort,
-    secure: false, // nếu dùng port 465 (smtps) thì để true, còn lại hãy để false cho tất cả các port khác
+    secure: true, // nếu dùng port 465 (smtps) thì để true, còn lại hãy để false cho tất cả các port khác
     auth: {
       user: adminEmail,
       pass: adminPassword
