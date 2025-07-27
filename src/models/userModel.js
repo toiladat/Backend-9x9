b5fbbaec396800b9de4b6be7083a5da2ec56e865
PhamTienDat <tdat9663@gmail.com>
2025-07-28 01:46:38 +0700

    fix approve

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -178,6 +178,25 @@ const distributeAmounts = async (receivers) => {
   }
 }
 
+const findDistributedUser = async (inviterAddress, boxNumber) => {
+  try {
+    const db = GET_DB().collection(USER_COLLECTION_NAME)
+    const addressToCheck = inviterAddress
+
+    const user = await db.findOne(
+      { address: addressToCheck },
+      { projection: { openBoxHistories: 1, address: 1 } }
+    )
+    const isBoxOpened = user?.openBoxHistories?.some(
+      history => history.boxNumber === boxNumber && history.open === true
+    )
+
+    return isBoxOpened ? user : { address: process.env.SYSTEM_ADDRESS }
+  } catch (error) {
+    throw error
+  }
+}
+
 export const userModel = {
   USER_COLLECTION_NAME,
   USER_COLLECTION_SCHEMA,
@@ -189,5 +208,6 @@ export const userModel = {
   getUsers,
   updateUserByAdderss,
   openBox,
-  distributeAmounts
+  distributeAmounts,
+  findDistributedUser
 }
